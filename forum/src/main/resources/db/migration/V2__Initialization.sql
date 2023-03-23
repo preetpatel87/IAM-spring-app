f8a619230ffde56ed3c6449e099c64cc1255f665
Preet Patel <preet.patel2@ontariotechu.net>
2023-03-23 11:53:23 -0400

    Editing SQL script

--- forum/src/main/resources/db/migration/V2__Initialization.sql
+++ forum/src/main/resources/db/migration/V2__Initialization.sql
@@ -6,4 +6,4 @@ INSERT INTO users (name, username, password, role) VALUES ('tester3', 'testuser3
 INSERT into rbac (role, permissions) VALUES ('Viewer', ARRAY ['getThread','getThreadList','getCommentsList','createComment']);
 INSERT into rbac (role, permissions) VALUES ('Creator', ARRAY ['getThread','getThreadList','createThread','getCommentsList','createComment']);
 INSERT into rbac (role, permissions) VALUES ('Moderator', ARRAY ['getThread','getThreadList','createThread','deleteThread','getCommentsList','createComment','deleteComment']);
-INSERT into rbac (role, permissions) VALUES ('Admin', ARRAY ['getThread','getThreadList','createThread','editThread','deleteThread','getCommentsList','createComment','editComment','deleteComment']);
+INSERT into rbac (role, permissions) VALUES ('Admin', ARRAY ['getThread','getThreadList','createThread','editThread','deleteThread','getCommentsList','createComment','editComment','deleteComment','editUser','getUserList']);
