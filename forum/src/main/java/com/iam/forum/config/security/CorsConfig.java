caf153b6a86be11d17dcff732be3f9cbd73e6a2d
Preet Patel <preet.patel2@ontariotechu.net>
2023-03-24 20:11:48 -0400

    Changing CORS config

--- forum/src/main/java/com/iam/forum/config/security/CorsConfig.java
+++ forum/src/main/java/com/iam/forum/config/security/CorsConfig.java
@@ -1,19 +1,15 @@
-package com.iam.forum.config.security;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class CorsConfig implements WebMvcConfigurer {
-    private final static long MAX_AGE_SECS = 3600;
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedHeaders("*")
-                .allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE")
-                .maxAge(MAX_AGE_SECS);
-    }
-}
+//package com.iam.forum.config.security;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class CorsConfig implements WebMvcConfigurer {
+//    private final static long MAX_AGE_SECS = 3600;
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//
+//    }
+//}
--- forum/src/main/java/com/iam/forum/config/security/WebMvcConfig.java
+++ forum/src/main/java/com/iam/forum/config/security/WebMvcConfig.java
@@ -4,10 +4,13 @@ import com.iam.forum.model.repository.RBACRepository;
 import com.iam.forum.model.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer {
 
     @Autowired
@@ -16,9 +19,22 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Autowired
     private UserRepository userRepository;
 
+    private final static long MAX_AGE_SECS = 3600;
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new ForumServiceInterceptor(rbacRepository, userRepository));
     }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE")
+                .maxAge(MAX_AGE_SECS);
+    }
+
+
 }
 
